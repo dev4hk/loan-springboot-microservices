938f098684e9150579d06d091398c8e33f8f9ca6
Hoon <hoon0661@gmail.com>
2024-12-24 11:28:56 -0500

    created initial counsel server, added lombok version number manually due to lombok issue where plugin and annotation processing not working

---
+++ counsel-server/src/test/java/com/example/counselserver/CounselServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.counselserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CounselServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
