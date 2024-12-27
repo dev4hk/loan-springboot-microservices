81d2b7cfe9d8134cb6e93f9389da148dba3612b5
Hoon <hoon0661@gmail.com>
2024-12-27 13:12:38 -0500

    added swagger documentation on terms service

--- terms-server/src/main/java/com/example/termsserver/controller/TermsController.java
+++ terms-server/src/main/java/com/example/termsserver/controller/TermsController.java
@@ -4,6 +4,11 @@ import com.example.termsserver.dto.ResponseDTO;
 import com.example.termsserver.dto.TermsRequestDto;
 import com.example.termsserver.dto.TermsResponseDto;
 import com.example.termsserver.service.ITermsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -16,7 +21,7 @@ import static com.example.termsserver.dto.ResponseDTO.ok;
 
 @Tag(
         name = "CRUD REST APIs for Terms",
-        description = "CRUD REST APIs to CREATE, UPDATE, FETCH AND DELETE counsel details"
+        description = "CRUD REST APIs to CREATE, UPDATE, FETCH AND DELETE terms details"
 )
 @Validated
 @RequiredArgsConstructor
@@ -26,26 +31,130 @@ public class TermsController {
 
     private final ITermsService termsService;
 
+    @Operation(
+            summary = "Create Terms REST API",
+            description = "REST API to create new terms"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "HTTP Status Bad Request",
+                    content = @Content(
+                            schema = @Schema(implementation = ResponseDTO.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error"
+            )
+    }
+    )
     @PostMapping
     public ResponseDTO<TermsResponseDto> create(@Valid @RequestBody TermsRequestDto request) {
         return ok(termsService.create(request));
     }
 
+    @Operation(
+            summary = "Get Terms REST API",
+            description = "REST API to get all terms"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error"
+            )
+    }
+    )
     @GetMapping
     public ResponseDTO<List<TermsResponseDto>> getAll() {
         return ok(termsService.getAll());
     }
 
+    @Operation(
+            summary = "Get Terms REST API",
+            description = "REST API to get terms"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "HTTP Status Bad Request",
+                    content = @Content(
+                            schema = @Schema(implementation = ResponseDTO.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error"
+            )
+    }
+    )
     @GetMapping("/{termsId}")
     public ResponseDTO<TermsResponseDto> get(@PathVariable Long termsId) {
         return ok(termsService.get(termsId));
     }
 
+
+    @Operation(
+            summary = "Update Terms REST API",
+            description = "REST API to update terms"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "HTTP Status Bad Request",
+                    content = @Content(
+                            schema = @Schema(implementation = ResponseDTO.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error"
+            )
+    }
+    )
     @PutMapping("/{termsId}")
     public ResponseDTO<TermsResponseDto> update(@PathVariable Long termsId, @Valid @RequestBody TermsRequestDto request) {
         return ok(termsService.update(termsId, request));
     }
 
+    @Operation(
+            summary = "Delete Terms REST API",
+            description = "REST API to delete terms"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "HTTP Status Bad Request",
+                    content = @Content(
+                            schema = @Schema(implementation = ResponseDTO.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error"
+            )
+    }
+    )
     @DeleteMapping("/{termsId}")
     public ResponseDTO<TermsResponseDto> delete(@PathVariable Long termsId) {
         termsService.delete(termsId);
--- terms-server/src/main/java/com/example/termsserver/dto/TermsRequestDto.java
+++ terms-server/src/main/java/com/example/termsserver/dto/TermsRequestDto.java
@@ -1,5 +1,6 @@
 package com.example.termsserver.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
@@ -11,10 +12,16 @@ import lombok.*;
 @Setter
 public class TermsRequestDto {
 
+    @Schema(
+            description = "Name", example = "terms"
+    )
     @NotBlank(message = "Name cannot be empty")
     @Size(max = 255, message = "Name must be less than 255 characters")
     private String name;
 
+    @Schema(
+            description = "Terms detail URL", example = "https://randomTermsPage"
+    )
     @NotBlank(message = "Terms detail URL cannot be empty")
     @Size(max = 255, message = "Terms detail URL must be less than 255 characters")
     private String termsDetailUrl;
