75149d1bf3d1a6d4095b8e167dcf8e75dccdd5d0
Hoon <hoon0661@gmail.com>
2024-12-27 14:53:53 -0500

    created initial accept terms server

---
+++ accept-terms-server/src/main/java/com/example/accepttermsserver/AcceptTermsServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.accepttermsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcceptTermsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcceptTermsServerApplication.class, args);
+    }
+
+}
