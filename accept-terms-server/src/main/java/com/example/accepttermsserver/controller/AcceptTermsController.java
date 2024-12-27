f8ce73c2bfae20ca5e10c9d28d16d33dbabfb2d6
Hoon <hoon0661@gmail.com>
2024-12-27 17:45:57 -0500

    added controller test case for accept terms

---
+++ accept-terms-server/src/main/java/com/example/accepttermsserver/controller/AcceptTermsController.java
@@ -0,0 +1,22 @@
+package com.example.accepttermsserver.controller;
+
+import com.example.accepttermsserver.dto.AcceptTermsRequestDto;
+import com.example.accepttermsserver.dto.ResponseDTO;
+import com.example.accepttermsserver.service.IAcceptTermsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/accept-terms")
+@RestController
+public class AcceptTermsController {
+
+    private final IAcceptTermsService acceptTermsService;
+
+    @PostMapping
+    public ResponseDTO<?> create(AcceptTermsRequestDto acceptTermsRequestDto) {
+        return null;
+    }
+}
