0c600bbe94b77a362333069ea1785485379e8f62
Hoon <hoon0661@gmail.com>
2024-12-27 08:51:32 -0500

    created initial terms service

---
+++ terms-server/src/main/java/com/example/termsserver/TermsServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.termsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TermsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TermsServerApplication.class, args);
+    }
+
+}
---
+++ terms-server/src/test/java/com/example/termsserver/TermsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.termsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TermsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
