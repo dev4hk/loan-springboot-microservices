1f00a0497b80652407b7369728aa61475b7f7ddc
Hoon <hoon0661@gmail.com>
2024-12-29 18:43:01 -0500

    created initial judgement server

---
+++ judgement-server/src/test/java/com/example/judgement_server/JudgementServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.judgement_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JudgementServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
