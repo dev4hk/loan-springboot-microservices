28199b07cbb6b6d2eb0159d6434bd1d60bf38474
Hoon <hoon0661@gmail.com>
2024-12-29 15:31:54 -0500

    added documentation on file service

--- file-storage-server/src/main/java/com/example/filestorageserver/FileStorageServerApplication.java
+++ file-storage-server/src/main/java/com/example/filestorageserver/FileStorageServerApplication.java
@@ -1,8 +1,21 @@
 package com.example.filestorageserver;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "File storage microservice",
+                description = "File storage microservice REST API Documentation",
+                version = "v1"
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "File storage microservice REST API Documentation"
+        )
+)
 @SpringBootApplication
 public class FileStorageServerApplication {
 
--- file-storage-server/src/main/java/com/example/filestorageserver/controller/FileStorageController.java
+++ file-storage-server/src/main/java/com/example/filestorageserver/controller/FileStorageController.java
@@ -3,6 +3,12 @@ package com.example.filestorageserver.controller;
 import com.example.filestorageserver.dto.FileResponseDto;
 import com.example.filestorageserver.dto.ResponseDTO;
 import com.example.filestorageserver.service.IFileStorageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -15,6 +21,10 @@ import java.util.List;
 
 import static com.example.filestorageserver.dto.ResponseDTO.ok;
 
+@Tag(
+        name = "CRUD REST APIs for File Storage",
+        description = "REST APIs to upload, download, fetch file info, delete files"
+)
 @RequiredArgsConstructor
 @RequestMapping("/files")
 @RestController
@@ -22,12 +32,56 @@ public class FileStorageController {
 
     private final IFileStorageService fileStorageService;
 
+    @Operation(
+            summary = "Upload file REST API",
+            description = "REST API to upload file"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "HTTP Status Bad Request",
+                    content = @Content(
+                            schema = @Schema(implementation = ResponseDTO.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error"
+            )
+    }
+    )
     @PostMapping("/{applicationId}")
     public ResponseDTO<Void> upload(@PathVariable Long applicationId, MultipartFile file) {
         fileStorageService.save(applicationId, file);
         return ok();
     }
 
+    @Operation(
+            summary = "Download file REST API",
+            description = "REST API to download file"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "HTTP Status Bad Request",
+                    content = @Content(
+                            schema = @Schema(implementation = ResponseDTO.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error"
+            )
+    }
+    )
     @GetMapping("/{applicationId}")
     public ResponseEntity<Resource> download(@PathVariable Long applicationId, @RequestParam(value = "fileName") String fileName) {
         Resource file = fileStorageService.load(applicationId, fileName);
@@ -38,6 +92,28 @@ public class FileStorageController {
                 .body(file);
     }
 
+    @Operation(
+            summary = "Fetch file info REST API",
+            description = "REST API to fetch file info"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "HTTP Status Bad Request",
+                    content = @Content(
+                            schema = @Schema(implementation = ResponseDTO.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error"
+            )
+    }
+    )
     @GetMapping("/{applicationId}/info")
     public ResponseDTO<List<FileResponseDto>> getFilesInfo(@PathVariable Long applicationId) {
         List<FileResponseDto> filesInfo = fileStorageService.loadAll(applicationId).map(path -> {
@@ -50,6 +126,28 @@ public class FileStorageController {
         return ok(filesInfo);
     }
 
+    @Operation(
+            summary = "Delete files REST API",
+            description = "REST API to delete files"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "HTTP Status Bad Request",
+                    content = @Content(
+                            schema = @Schema(implementation = ResponseDTO.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error"
+            )
+    }
+    )
     @DeleteMapping("/{applicationId}")
     public ResponseDTO<Void> deleteAll(@PathVariable Long applicationId) {
         fileStorageService.deleteAll(applicationId);
