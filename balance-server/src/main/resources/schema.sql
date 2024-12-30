2ef9b17a3094558b0c667ca89d2d898e48dce65f
Hoon <hoon0661@gmail.com>
2024-12-30 15:01:01 -0500

    added schema.sql and balance entity

---
+++ balance-server/src/main/resources/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS Balance (
+    balanceId BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    applicationId BIGINT NOT NULL,
+    balance DECIMAL(15, 2) NOT NULL,
+    created_at date NOT NULL,
+    created_by varchar(20) NOT NULL,
+    updated_at date DEFAULT NULL,
+    updated_by varchar(20) DEFAULT NULL,
+    is_deleted BOOLEAN DEFAULT FALSE
+);
