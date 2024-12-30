4ad0c92ca0c46288c3f50d2ebb6659d128cfa69c
Hoon <hoon0661@gmail.com>
2024-12-30 11:29:13 -0500

    added swagger docs on judgement server

--- judgement-server/src/main/java/com/example/judgement_server/JudgementServerApplication.java
+++ judgement-server/src/main/java/com/example/judgement_server/JudgementServerApplication.java
@@ -1,10 +1,23 @@
 package com.example.judgement_server;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Judgement microservice",
+				description = "Judgement microservice REST API Documentation",
+				version = "v1"
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Judgement microservice REST API Documentation"
+		)
+)
 @EnableFeignClients
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @SpringBootApplication
--- judgement-server/src/main/java/com/example/judgement_server/dto/GrantAmountDto.java
+++ judgement-server/src/main/java/com/example/judgement_server/dto/GrantAmountDto.java
@@ -1,9 +1,14 @@
 package com.example.judgement_server.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.math.BigDecimal;
 
+@Schema(
+        name = "Grant Amount",
+        description = "Schema to hold grant amount information"
+)
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -11,8 +16,14 @@ import java.math.BigDecimal;
 @Setter
 public class GrantAmountDto {
 
+    @Schema(
+            description = "Application Id", example = "1L"
+    )
     private Long applicationId;
 
+    @Schema(
+            description = "Approval amount", example = "100.00"
+    )
     private BigDecimal approvalAmount;
 
 }
