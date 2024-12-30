5ce5517c11019a64a58469338a368f6c0609a952
Hoon <hoon0661@gmail.com>
2024-12-30 15:04:16 -0500

    added common dtos, exception handling in balance server

--- balance-server/src/main/java/com/example/balance_server/BalanceServerApplication.java
+++ balance-server/src/main/java/com/example/balance_server/BalanceServerApplication.java
@@ -1,8 +1,23 @@
 package com.example.balance_server;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Balance microservice",
+				description = "Balance microservice REST API Documentation",
+				version = "v1"
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Balance microservice REST API Documentation"
+		)
+)
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @SpringBootApplication
 public class BalanceServerApplication {
 
---
+++ balance-server/src/main/java/com/example/balance_server/audit/AuditAwareImpl.java
@@ -0,0 +1,16 @@
+package com.example.balance_server.audit;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("auditAwareImpl")
+public class AuditAwareImpl implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("APPLICATION_MS");
+    }
+	
+}
