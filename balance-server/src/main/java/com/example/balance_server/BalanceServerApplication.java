1c53e0ff8cad592264037b1f7b51f01cd692e608
Hoon <hoon0661@gmail.com>
2024-12-30 14:03:32 -0500

    created initial balance service

---
+++ balance-server/src/main/java/com/example/balance_server/BalanceServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.balance_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalanceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalanceServerApplication.class, args);
+	}
+
+}
---
+++ balance-server/src/test/java/com/example/balance_server/BalanceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.balance_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalanceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
