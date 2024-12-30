c6f2a459da062518be7575a472240df5e330474d
Hoon <hoon0661@gmail.com>
2024-12-30 13:24:36 -0500

    created initial entry server

---
+++ entry-server/src/main/java/com/example/entry_server/EntryServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.entry_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntryServerApplication.class, args);
+	}
+
+}
---
+++ entry-server/src/test/java/com/example/entry_server/EntryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.entry_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
