b91b21b625e76d5ebebfc39294c28ca951cd73a5
Hoon <hoon0661@gmail.com>
2025-01-01 11:58:28 -0500

    added swagger ui on entry server

--- entry-server/src/main/java/com/example/entry_server/dto/EntryRequestDto.java
+++ entry-server/src/main/java/com/example/entry_server/dto/EntryRequestDto.java
@@ -1,11 +1,16 @@
 package com.example.entry_server.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.math.BigDecimal;
 
+@Schema(
+        name = "Entry Request",
+        description = "Schema to hold entry information"
+)
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -13,6 +18,9 @@ import java.math.BigDecimal;
 @Setter
 public class EntryRequestDto {
 
+    @Schema(
+            description = "entry", example = "1000.00"
+    )
     @NotNull(message = "Entry amount cannot be null or empty")
     @Digits(integer = 15, fraction = 2, message = "Entry amount must be a number with up to 2 decimal places")
     private BigDecimal entryAmount;
