005f45e1fe0741e968728bdabc89dcad7d37b105
Hoon <hoon0661@gmail.com>
2025-01-01 14:16:23 -0500

    added schema.sql and repayment entity

---
+++ repayment-server/src/main/java/com/example/repayment_server/entity/Repayment.java
@@ -0,0 +1,28 @@
+package com.example.repayment_server.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.SQLRestriction;
+import org.hibernate.annotations.Where;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@SQLRestriction("is_deleted=false")
+public class Repayment extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long repaymentId;
+
+    private Long applicationId;
+
+    private BigDecimal repaymentAmount;
+}
