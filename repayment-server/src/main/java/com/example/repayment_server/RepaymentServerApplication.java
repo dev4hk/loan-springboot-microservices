3c2d31006cd770b52409b0a45987b722112985a0
Hoon <hoon0661@gmail.com>
2025-01-01 12:08:46 -0500

    created initial repayment server

---
+++ repayment-server/src/main/java/com/example/repayment_server/RepaymentServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.repayment_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepaymentServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepaymentServerApplication.class, args);
+	}
+
+}
---
+++ repayment-server/src/test/java/com/example/repayment_server/RepaymentServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.repayment_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepaymentServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
