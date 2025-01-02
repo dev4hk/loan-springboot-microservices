7fb476eaca1c96f365b8b60421e80cc6b91db9fd
Hoon <hoon0661@gmail.com>
2025-01-02 10:24:34 -0500

    added swagger doc in repayment server

--- repayment-server/src/main/java/com/example/repayment_server/controller/RepaymentController.java
+++ repayment-server/src/main/java/com/example/repayment_server/controller/RepaymentController.java
@@ -2,6 +2,12 @@ package com.example.repayment_server.controller;
 
 import com.example.repayment_server.dto.*;
 import com.example.repayment_server.service.IRepaymentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
@@ -11,6 +17,10 @@ import java.util.List;
 
 import static com.example.repayment_server.dto.ResponseDTO.ok;
 
+@Tag(
+        name = "CRUD REST APIs for Repayment",
+        description = "CRUD REST APIs to CREATE, UPDATE, FETCH AND DELETE repayment details"
+)
 @Validated
 @RestController
 @RequestMapping("/repayments")
@@ -19,6 +29,28 @@ public class RepaymentController {
 
     private final IRepaymentService repaymentService;
 
+    @Operation(
+            summary = "Create Repayment REST API",
+            description = "REST API to create repayment"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "HTTP Status Bad Request",
+                    content = @Content(
+                            schema = @Schema(implementation = ResponseDTO.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error"
+            )
+    }
+    )
     @PostMapping("/{applicationId}")
     public ResponseDTO<RepaymentResponseDto> createRepayment(
             @PathVariable Long applicationId,
@@ -27,6 +59,28 @@ public class RepaymentController {
         return ok(responseDto);
     }
 
+    @Operation(
+            summary = "Get Repayments REST API",
+            description = "REST API to get repayments"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "HTTP Status Bad Request",
+                    content = @Content(
+                            schema = @Schema(implementation = ResponseDTO.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error"
+            )
+    }
+    )
     @GetMapping("/{applicationId}")
     public ResponseDTO<List<RepaymentListResponseDto>> getRepayments(
             @PathVariable Long applicationId) {
@@ -34,6 +88,28 @@ public class RepaymentController {
         return ok(responseDtos);
     }
 
+    @Operation(
+            summary = "Update Repayment REST API",
+            description = "REST API to update repayment"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "HTTP Status Bad Request",
+                    content = @Content(
+                            schema = @Schema(implementation = ResponseDTO.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error"
+            )
+    }
+    )
     @PutMapping("/{repaymentId}")
     public ResponseDTO<RepaymentUpdateResponseDto> updateRepayment(
             @PathVariable Long repaymentId,
@@ -42,6 +118,28 @@ public class RepaymentController {
         return ok(responseDto);
     }
 
+    @Operation(
+            summary = "Celete Repayment REST API",
+            description = "REST API to delete repayment"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "HTTP Status Bad Request",
+                    content = @Content(
+                            schema = @Schema(implementation = ResponseDTO.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error"
+            )
+    }
+    )
     @DeleteMapping("/{repaymentId}")
     public ResponseDTO<Void> deleteRepayment(
             @PathVariable Long repaymentId) {
--- repayment-server/src/main/java/com/example/repayment_server/dto/RepaymentRequestDto.java
+++ repayment-server/src/main/java/com/example/repayment_server/dto/RepaymentRequestDto.java
@@ -1,11 +1,16 @@
 package com.example.repayment_server.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.math.BigDecimal;
 
+@Schema(
+        name = "Repayment Request",
+        description = "Schema to hold repayment information"
+)
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -13,6 +18,9 @@ import java.math.BigDecimal;
 @Setter
 public class RepaymentRequestDto {
 
+    @Schema(
+            description = "Repayment amount"
+    )
     @NotNull(message = "Repayment amount cannot be null or empty")
     @Digits(integer = 15, fraction = 2, message = "Repayment amount must be a number with up to 2 decimal places")
     private BigDecimal repaymentAmount;
