ecce1866ee308db159efbd602f55787ef6a1b84c
Hoon <hoon0661@gmail.com>
2025-01-03 10:35:53 -0500

    added validation on dtos to have amount to be at least positive

--- balance-server/src/main/java/com/example/balance_server/dto/BalanceUpdateRequestDto.java
+++ balance-server/src/main/java/com/example/balance_server/dto/BalanceUpdateRequestDto.java
@@ -1,6 +1,7 @@
 package com.example.balance_server.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
@@ -29,6 +30,7 @@ public class BalanceUpdateRequestDto {
     )
     @NotNull(message = "Entry amount cannot be null or empty")
     @Digits(integer = 15, fraction = 2, message = "Before entry amount must be a number with up to 2 decimal places")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Before entry amount must be greater than zero")
     private BigDecimal beforeEntryAmount;
 
     @Schema(
@@ -36,6 +38,7 @@ public class BalanceUpdateRequestDto {
     )
     @NotNull(message = "Entry amount cannot be null or empty")
     @Digits(integer = 15, fraction = 2, message = "After entry amount must be a number with up to 2 decimal places")
+    @DecimalMin(value = "0.0", inclusive = false, message = "After entry amount must be greater than zero")
     private BigDecimal afterEntryAmount;
 
 }
