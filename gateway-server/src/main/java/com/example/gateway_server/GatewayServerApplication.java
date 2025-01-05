d2dc7410dd6248443fe87b02c42fd1c9e9f50d57
Hoon <hoon0661@gmail.com>
2025-01-05 17:08:23 -0500

    added fallback controller into circuit breaker filter

--- gateway-server/src/main/java/com/example/gateway_server/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/example/gateway_server/GatewayServerApplication.java
@@ -11,69 +11,87 @@ import java.time.LocalDateTime;
 @SpringBootApplication
 public class GatewayServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServerApplication.class, args);
+    }
 
-	@Bean
-	public RouteLocator loanRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
-		return routeLocatorBuilder.routes()
-				.route(p -> p
-						.path("/loan/accept-terms/**")
-						.filters( f -> f.rewritePath("/loan/accept-terms/(?<segment>.*)","/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
-								.circuitBreaker(config -> config.setName("acceptTermsCircuitBreaker"))
-						)
-						.uri("lb://ACCEPT-TERMS-SERVER"))
-				.route(p -> p
-						.path("/loan/application/**")
-						.filters( f -> f.rewritePath("/loan/application/(?<segment>.*)","/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
-								.circuitBreaker(config -> config.setName("applicationCircuitBreaker")))
-						.uri("lb://APPLICATION-SERVER"))
-				.route(p -> p
-						.path("/loan/balance/**")
-						.filters( f -> f.rewritePath("/loan/balance/(?<segment>.*)","/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
-						.circuitBreaker(config -> config.setName("balanceCircuitBreaker")))
-						.uri("lb://BALANCE-SERVER"))
-				.route(p -> p
-						.path("/loan/counsel/**")
-						.filters( f -> f.rewritePath("/loan/counsel/(?<segment>.*)","/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
-						.circuitBreaker(config -> config.setName("counselCircuitBreaker")))
-						.uri("lb://COUNSEL-SERVER"))
-				.route(p -> p
-						.path("/loan/entry/**")
-						.filters( f -> f.rewritePath("/loan/entry/(?<segment>.*)","/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
-						.circuitBreaker(config -> config.setName("entryCircuitBreaker")))
-						.uri("lb://ENTRY-SERVER"))
-				.route(p -> p
-						.path("/loan/file-storage/**")
-						.filters( f -> f.rewritePath("/loan/file-storage/(?<segment>.*)","/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
-						.circuitBreaker(config -> config.setName("fileStorageCircuitBreaker")))
-						.uri("lb://FILE-STORAGE-SERVER"))
-				.route(p -> p
-						.path("/loan/judgement/**")
-						.filters( f -> f.rewritePath("/loan/judgement/(?<segment>.*)","/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
-						.circuitBreaker(config -> config.setName("judgementCircuitBreaker")))
-						.uri("lb://JUDGEMENT-SERVER"))
-				.route(p -> p
-						.path("/loan/repayment/**")
-						.filters( f -> f.rewritePath("/loan/repayment/(?<segment>.*)","/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
-						.circuitBreaker(config -> config.setName("repaymentCircuitBreaker")))
-						.uri("lb://REPAYMENT-SERVER"))
-				.route(p -> p
-						.path("/loan/terms/**")
-						.filters( f -> f.rewritePath("/loan/terms/(?<segment>.*)","/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
-						.circuitBreaker(config -> config.setName("termsCircuitBreaker")))
-						.uri("lb://TERMS-SERVER"))
-				.build();
-	}
+    @Bean
+    public RouteLocator loanRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(p -> p
+                        .path("/loan/accept-terms/**")
+                        .filters(f -> f.rewritePath("/loan/accept-terms/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .circuitBreaker(config -> config.setName("acceptTermsCircuitBreaker")
+                                        .setFallbackUri("forward:/error")
+                                )
+                        )
+                        .uri("lb://ACCEPT-TERMS-SERVER"))
+                .route(p -> p
+                        .path("/loan/application/**")
+                        .filters(f -> f.rewritePath("/loan/application/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .circuitBreaker(config -> config.setName("applicationCircuitBreaker")
+                                        .setFallbackUri("forward:/error")
+                                ))
+                        .uri("lb://APPLICATION-SERVER"))
+                .route(p -> p
+                        .path("/loan/balance/**")
+                        .filters(f -> f.rewritePath("/loan/balance/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .circuitBreaker(config -> config.setName("balanceCircuitBreaker")
+                                        .setFallbackUri("forward:/error")
+                                ))
+                        .uri("lb://BALANCE-SERVER"))
+                .route(p -> p
+                        .path("/loan/counsel/**")
+                        .filters(f -> f.rewritePath("/loan/counsel/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .circuitBreaker(config -> config.setName("counselCircuitBreaker")
+                                        .setFallbackUri("forward:/error")
+                                ))
+                        .uri("lb://COUNSEL-SERVER"))
+                .route(p -> p
+                        .path("/loan/entry/**")
+                        .filters(f -> f.rewritePath("/loan/entry/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .circuitBreaker(config -> config.setName("entryCircuitBreaker")
+                                        .setFallbackUri("forward:/error")
+                                ))
+                        .uri("lb://ENTRY-SERVER"))
+                .route(p -> p
+                        .path("/loan/file-storage/**")
+                        .filters(f -> f.rewritePath("/loan/file-storage/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .circuitBreaker(config -> config.setName("fileStorageCircuitBreaker")
+                                        .setFallbackUri("forward:/error")
+                                ))
+                        .uri("lb://FILE-STORAGE-SERVER"))
+                .route(p -> p
+                        .path("/loan/judgement/**")
+                        .filters(f -> f.rewritePath("/loan/judgement/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .circuitBreaker(config -> config.setName("judgementCircuitBreaker")
+                                        .setFallbackUri("forward:/error")
+                                ))
+                        .uri("lb://JUDGEMENT-SERVER"))
+                .route(p -> p
+                        .path("/loan/repayment/**")
+                        .filters(f -> f.rewritePath("/loan/repayment/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .circuitBreaker(config -> config.setName("repaymentCircuitBreaker")
+                                        .setFallbackUri("forward:/error")
+                                ))
+                        .uri("lb://REPAYMENT-SERVER"))
+                .route(p -> p
+                        .path("/loan/terms/**")
+                        .filters(f -> f.rewritePath("/loan/terms/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .circuitBreaker(config -> config.setName("termsCircuitBreaker")
+                                        .setFallbackUri("forward:/error")
+                                ))
+                        .uri("lb://TERMS-SERVER"))
+                .build();
+    }
 
 }
