50004ebbf899ca04714e2505bb75d1d7d0e4f3e7
Hoon <hoon0661@gmail.com>
2025-01-30 12:42:18 -0500

    added image slider component

---
+++ ms-loan-frontend/src/app/components/image-slider/image-slider.component.ts
@@ -0,0 +1,37 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { ImageSlideInterface } from '../../interfaces/image-slide-interface';
+import { slideAnimation } from './animations';
+
+@Component({
+  selector: 'app-image-slider',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './image-slider.component.html',
+  styleUrls: ['./image-slider.component.scss'],
+  animations: [slideAnimation],
+})
+export class ImageSliderComponent {
+  @Input() slides: Array<ImageSlideInterface> = [];
+  currentIndex: number = 0;
+
+  getCurrentSlideUrl(): string {
+    return `url('${this.slides[this.currentIndex].url}')`;
+  }
+
+  goToNext(): void {
+    const isLastSlide = this.currentIndex === this.slides.length - 1;
+    this.currentIndex = isLastSlide ? 0 : this.currentIndex + 1;
+  }
+
+  goToPrevious(): void {
+    const isFirstSlide = this.currentIndex === 0;
+    this.currentIndex = isFirstSlide
+      ? this.slides.length - 1
+      : this.currentIndex - 1;
+  }
+
+  goToSlide(slideIndex: number): void {
+    this.currentIndex = slideIndex;
+  }
+}
---
+++ ms-loan-frontend/src/app/interfaces/image-slide-interface.ts
@@ -0,0 +1,5 @@
+export interface ImageSlideInterface {
+  url: string;
+  title: string;
+  description: string;
+}
