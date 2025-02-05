b67efec57e1bde008f55818a2923f6679510e191
Hoon <hoon0661@gmail.com>
2025-02-04 20:46:22 -0500

    added image slider to be displayed when stat donut is not available

--- ms-loan-frontend/src/app/pages/home/home.component.ts
+++ ms-loan-frontend/src/app/pages/home/home.component.ts
@@ -1,26 +1,96 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DonutChartComponent } from '../../components/donut-chart/donut-chart.component';
 import { DonutChartInterface } from '../../components/donut-chart/donut-chart-interface';
+import { RepaymentService } from '../../services/repayment.service';
+import { RepaymentResponseDto } from '../../dtos/repayment-response-dto';
+import { ApplicationService } from '../../services/application.service';
+import { ApplicationResponseDto } from '../../dtos/application-response-dto';
+import { ImageSliderComponent } from '../../components/image-slider/image-slider.component';
+import { homeImageSlides } from '../../components/image-slider/home-image-slides';
+import { ImageSlideInterface } from '../../components/image-slider/image-slide-interface';
+import { RouterModule } from '@angular/router';
+import { KeycloakService } from '../../utils/keycloak/keycloak.service';
+import { CounselResponseDto } from '../../dtos/counsel-response-dto';
+import { CounselService } from '../../services/counsel.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [DonutChartComponent],
+  imports: [
+    DonutChartComponent,
+    ImageSliderComponent,
+    RouterModule,
+    CommonModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-  data1: DonutChartInterface = {
-    title: 'title1',
-    data: [
-      { label: 'data1', number: 10 },
-      { label: 'data2', number: 20 },
-    ],
-  };
-  data2: DonutChartInterface = {
-    title: 'title2',
-    data: [
-      { label: 'data2', number: 10 },
-      { label: 'data2', number: 20 },
-    ],
-  };
+export class HomeComponent implements OnInit {
+  repayments?: Array<RepaymentResponseDto>;
+  application?: ApplicationResponseDto;
+  applicationId?: number;
+  counsel?: CounselResponseDto;
+  dataChart?: DonutChartInterface;
+  slides: Array<ImageSlideInterface> = homeImageSlides;
+
+  constructor(
+    private applicationService: ApplicationService,
+    private counselService: CounselService,
+    private repaymentService: RepaymentService,
+    private keycloakService: KeycloakService
+  ) {}
+
+  ngOnInit(): void {
+    this.getApplicationByEmail();
+    if (!this.application) {
+      this.getCounsel();
+    }
+  }
+
+  getApplicationByEmail() {
+    this.applicationService.getApplicationByEmail().subscribe({
+      next: (res) => {
+        this.application = res.data;
+        if (this.application?.applicationId) {
+          this.applicationId = this.applicationId;
+          this.getRepayments(this.applicationId!);
+        }
+      },
+      error: (res) => {
+        console.log(res.error);
+      },
+    });
+  }
+
+  getRepayments(applicationId: number) {
+    this.repaymentService.getRepayments(applicationId).subscribe({
+      next: (res) => {
+        this.repayments = res.data;
+        this.createChartData();
+      },
+      error: (res) => console.log(res.error),
+    });
+  }
+
+  getCounsel() {
+    this.counselService.getCounselByEmail().subscribe({
+      next: (res) => (this.counsel = res.data),
+      error: (res) => console.log(res.error),
+    });
+  }
+
+  createChartData() {
+    const balance = this.repayments![this.repayments!.length - 1].balance;
+    const totalRepaymentAmount = this.repayments!.reduce(
+      (sum, repayment) => sum + (repayment.repaymentAmount || 0),
+      0
+    );
+    this.dataChart = {
+      title: 'Current Balance vs. Repayment Amount',
+      data: [
+        { label: 'Balance', number: balance ?? 0 },
+        { label: 'Repayment Amount', number: totalRepaymentAmount },
+      ],
+    };
+  }
 }
