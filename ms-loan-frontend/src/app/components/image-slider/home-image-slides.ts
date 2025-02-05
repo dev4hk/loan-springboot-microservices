61a82bcf2447630db92f082660071536046cbdf0
Hoon <hoon0661@gmail.com>
2025-02-04 20:45:17 -0500

    updated data for image slider

---
+++ ms-loan-frontend/src/app/components/image-slider/home-image-slides.ts
@@ -0,0 +1,16 @@
+import { ImageSlideInterface } from './image-slide-interface';
+
+export const homeImageSlides: ImageSlideInterface[] = [
+  {
+    url: 'assets/images/home-image1.png',
+    title: 'Need Guidance Before Applying?',
+    description:
+      'Have questions? Our loan experts are here to help! Submit a consultation request, and we’ll reach out to assist you.',
+  },
+  {
+    url: 'assets/images/home-image2.png',
+    title: 'Ready to Apply for a Loan?',
+    description:
+      'Fill out the application form with your details and upload the required documents. A smooth and secure process to get started!',
+  },
+];
--- ms-loan-frontend/src/app/components/image-slider/loan-process-slides.ts
+++ ms-loan-frontend/src/app/components/image-slider/loan-process-slides.ts
@@ -2,37 +2,37 @@ import { ImageSlideInterface } from './image-slide-interface';
 
 export const loanProcessSlides: ImageSlideInterface[] = [
   {
-    url: 'assets/images/image1.jpg',
+    url: 'assets/images/home-image1.png',
     title: 'Step 1: Free Consultation (Optional)',
     description:
       'Have questions? Our loan experts are here to help! Submit a consultation request, and we’ll reach out to assist you.',
   },
   {
-    url: 'assets/images/image2.png',
+    url: 'assets/images/home-image2.png',
     title: 'Step 2: Apply for a Loan',
     description:
       'Fill out the application form with your details and upload the required documents. A smooth and secure process to get started!',
   },
   {
-    url: 'assets/images/image1.jpg',
+    url: 'assets/images/process-image3.png',
     title: 'Step 3: Expert Review & Loan Decision',
     description:
       'Our experts will review your application and determine how much you qualify for. You’ll receive a tailored loan offer.',
   },
   {
-    url: 'assets/images/image2.png',
+    url: 'assets/images/process-image4.png',
     title: 'Step 4: Review & Agreement',
     description:
       'Check the loan offer details carefully. If you agree with the terms, proceed to sign the loan contract.',
   },
   {
-    url: 'assets/images/image1.jpg',
+    url: 'assets/images/process-image5.png',
     title: 'Step 5: Receive Your Loan',
     description:
       'Once the contract is finalized, we’ll transfer the approved loan amount directly to your account.',
   },
   {
-    url: 'assets/images/image2.png',
+    url: 'assets/images/process-image6.png',
     title: 'Step 6: Repay & Complete Your Loan',
     description:
       'Make periodic repayments as per your plan. Once the full amount is paid, your loan process is complete!',
