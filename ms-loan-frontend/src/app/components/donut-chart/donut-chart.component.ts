bde8e09467ed4c43bf9011e6894a696973ff116a
Hoon <hoon0661@gmail.com>
2025-02-05 23:25:44 -0500

    changes layout of donut chart and admin home page, updated donut chart to accept & display empty data

--- ms-loan-frontend/src/app/components/donut-chart/donut-chart.component.ts
+++ ms-loan-frontend/src/app/components/donut-chart/donut-chart.component.ts
@@ -10,21 +10,19 @@ import {
   ApexTitleSubtitle,
 } from 'ng-apexcharts';
 import { DonutChartInterface } from './donut-chart-interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-donut-chart',
   standalone: true,
-  imports: [NgApexchartsModule],
+  imports: [NgApexchartsModule, CommonModule],
   templateUrl: './donut-chart.component.html',
   styleUrl: './donut-chart.component.scss',
 })
 export class DonutChartComponent implements OnChanges {
-  @Input() data: DonutChartInterface = {
-    title: '',
-    data: [],
-  };
+  @Input() data: DonutChartInterface = { title: '', data: [] };
 
-  public chartOptions!: {
+  public chartOptions: {
     series: ApexNonAxisChartSeries;
     chart: ApexChart;
     responsive: ApexResponsive[];
@@ -34,42 +32,79 @@ export class DonutChartComponent implements OnChanges {
     plotOptions: ApexPlotOptions;
     title: ApexTitleSubtitle;
     legend: ApexLegend;
+  } = {
+    title: { text: '', style: { color: '#29D0B2' }, margin: 20 },
+    legend: { show: false },
+    series: [],
+    labels: [],
+    chart: { type: 'donut', width: 360, height: 360 },
+    responsive: [
+      {
+        breakpoint: 480,
+        options: {
+          chart: { width: 250 },
+          legend: { position: 'bottom' },
+        },
+      },
+    ],
+    fill: { type: 'gradient' },
+    tooltip: {
+      enabled: false,
+      y: {
+        formatter: (val: number) => `${val}%`,
+        title: { formatter: (seriesName: string) => `${seriesName}:` },
+      },
+    },
+    plotOptions: {
+      pie: {
+        donut: {
+          labels: {
+            show: true,
+            name: { show: true, offsetY: -10, color: '#29D0B2' },
+            value: {
+              show: true,
+              formatter: (data) => `${data}%`,
+              color: '#29D0B2',
+            },
+          },
+        },
+      },
+    },
   };
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes['data'] && this.data.data.length) {
+    if (changes['data']) {
       this.updateChart();
     }
   }
 
   private updateChart() {
+    const hasData = this.data.data && this.data.data.length > 0;
+
     this.chartOptions = {
       title: {
-        text: this.data.title, // Use title from the input data
+        text: this.data.title,
         style: { color: '#29D0B2' },
         margin: 20,
       },
       legend: { show: false },
-      series: this.data.data.map((item) => item.number), // Accessing the array inside `data`
-      labels: this.data.data.map((item) => item.label),
-      chart: {
-        type: 'donut',
-        width: 330,
-        height: 330,
-      },
+      series: hasData ? this.data.data.map((item) => item.number) : [1],
+      labels: hasData ? this.data.data.map((item) => item.label) : ['No Data'],
+      chart: { type: 'donut', width: 360, height: 360 },
       responsive: [
         {
           breakpoint: 480,
           options: {
-            chart: { width: 200 },
+            chart: { width: 250 },
             legend: { position: 'bottom' },
           },
         },
       ],
       fill: { type: 'gradient' },
       tooltip: {
+        enabled: hasData,
         y: {
-          formatter: (val: number) => `${val}%`,
+          formatter: (val: number) => (hasData ? `${val}%` : 'No Data'),
           title: { formatter: (seriesName: string) => `${seriesName}:` },
         },
       },
@@ -81,7 +116,7 @@ export class DonutChartComponent implements OnChanges {
               name: { show: true, offsetY: -10, color: '#29D0B2' },
               value: {
                 show: true,
-                formatter: (data) => `${data}%`,
+                formatter: (data) => (hasData ? `${data}%` : 'No Data'),
                 color: '#29D0B2',
               },
             },
--- ms-loan-frontend/src/app/pages/admin/admin-home/admin-home.component.ts
+++ ms-loan-frontend/src/app/pages/admin/admin-home/admin-home.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DonutChartComponent } from '../../../components/donut-chart/donut-chart.component';
 import { DonutChartInterface } from '../../../components/donut-chart/donut-chart-interface';
 import { RouterModule } from '@angular/router';
+import { ApplicationService } from '../../../services/application.service';
+import { CounselService } from '../../../services/counsel.service';
+import { CommunicationStatus } from '../../../dtos/communitation-status';
 
 @Component({
   selector: 'app-admin-home',
@@ -9,21 +12,76 @@ import { RouterModule } from '@angular/router';
   templateUrl: './admin-home.component.html',
   styleUrl: './admin-home.component.scss',
 })
-export class AdminHomeComponent {
-  data1: DonutChartInterface = {
-    title: 'title1',
-    data: [{ label: 'data1', number: 1 }],
-  };
-  data2: DonutChartInterface = {
-    title: 'title2',
-    data: [{ label: 'data2', number: 1 }],
-  };
-  data3: DonutChartInterface = {
-    title: 'title3',
-    data: [{ label: 'data3', number: 1 }],
-  };
-  data4: DonutChartInterface = {
-    title: 'title4',
-    data: [{ label: 'data4', number: 1 }],
-  };
+export class AdminHomeComponent implements OnInit {
+  applicationStats?: Record<CommunicationStatus, number>;
+  counselStats?: Record<CommunicationStatus, number>;
+  applicationChart: DonutChartInterface = { title: '', data: [] };
+  counselChart: DonutChartInterface = { title: '', data: [] };
+  APPLICATION_CHART_TITLE = 'Application Statistics';
+  COUNSEL_CHART_TITLE = 'Counsel Statistics';
+
+  constructor(
+    private applicationService: ApplicationService,
+    private counselService: CounselService
+  ) {}
+
+  ngOnInit(): void {
+    this.getApplicationStats();
+    this.getCounselStats();
+  }
+
+  getApplicationStats() {
+    this.applicationService.getStats().subscribe({
+      next: (res) => {
+        this.applicationStats = res.data;
+
+        if (
+          this.applicationStats &&
+          Object.keys(this.applicationStats).length > 0
+        ) {
+          this.applicationChart = this.transformToDonutChartData(
+            this.APPLICATION_CHART_TITLE,
+            this.applicationStats
+          );
+        } else {
+          this.applicationChart = {
+            title: this.APPLICATION_CHART_TITLE,
+            data: [],
+          };
+        }
+      },
+      error: (res) => console.log(res.error),
+    });
+  }
+
+  getCounselStats() {
+    this.counselService.getStats().subscribe({
+      next: (res) => {
+        this.counselStats = res.data;
+
+        if (this.counselStats && Object.keys(this.counselStats).length > 0) {
+          this.counselChart = this.transformToDonutChartData(
+            this.COUNSEL_CHART_TITLE,
+            this.counselStats
+          );
+        } else {
+          this.counselChart = { title: this.COUNSEL_CHART_TITLE, data: [] };
+        }
+      },
+      error: (res) => console.log(res.error),
+    });
+  }
+
+  transformToDonutChartData(
+    title: string,
+    map: Record<CommunicationStatus, number>
+  ): DonutChartInterface {
+    return {
+      title: title,
+      data: Object.entries(map).map(([key, value]) => ({
+        label: key,
+        number: value,
+      })),
+    };
+  }
 }
